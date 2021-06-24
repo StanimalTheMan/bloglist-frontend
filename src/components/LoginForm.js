23eb2e7ad9ebfacb68e88fc64b39d0c6ea70726c
StanimalTheMan <sjc591@nyu.edu>
2021-06-24 00:20:00 -0400

    Blog list frontend, step 11 - define PropTypes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const LoginForm = ({
   handleSubmit,
@@ -35,32 +36,12 @@ const LoginForm = ({
   );
 };
 
-export default LoginForm;
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+};
 
-{
-  /* <div>
-<h2>Log in to application</h2>
-{notification}
-<form onSubmit={handleLogin}>
-  <div>
-    username
-    <input
-      type="text"
-      value={username}
-      name="Username"
-      onChange={({ target }) => setUsername(target.value)}
-    />
-  </div>
-  <div>
-    password
-    <input
-      type="password"
-      value={password}
-      name="Password"
-      onChange={({ target }) => setPassword(target.value)}
-    />
-  </div>
-  <button type="submit">login</button>
-</form>
-</div> */
-}
+export default LoginForm;
